0646118e189366afb920297421ffa5201ce1b6da
syshen <sysheen@gmail.com>
2012-09-04 09:56:05 +0800

    remove redundant NSLog

--- IRRemoteResourceDownloadOperation.m
+++ IRRemoteResourceDownloadOperation.m
@@ -225,7 +225,6 @@ NSString * const kIRRemoteResourceDownloadOperationURL = @"IRRemoteResourceDownl
 
 - (void) connectionDidFinishLoading:(NSURLConnection *)connection {
 
-    NSLog(@"Success: %@", [self underlyingRequest].URL);
 	if ([self isCancelled]) {
     if ([self.delegate respondsToSelector:@selector(remoteResourceDownloadOperationDidEnd:successfully:)])
       [self.delegate remoteResourceDownloadOperationDidEnd:self successfully:NO];
